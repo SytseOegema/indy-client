bec19ef0900bf446883fce8e746330df099376c2
Sytseoegema <sy.oegema@oegematransport.nl>
2020-05-17 14:45:42 +0200

    wallet test

--- Program.cs
+++ Program.cs
@@ -1,14 +1,39 @@
 ﻿using System;
 
+
+using Hyperledger.Indy.DidApi;
+using Hyperledger.Indy.Samples.Utils;
+using Hyperledger.Indy.WalletApi;
+
 namespace indyClient
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Wallet w = new Wallet();
-            w.test();
+            walletConfig = "{\"id\":\"Steward1\"}";
+            walletCredentails = "{\"key\":\"Steward1\"}";
+            importConfig = JsonConvert.SerializeObject(new
+                {
+                    path = "/home/hyper/wallets/steward_wallet",
+                    key = "Steward1"
+                });
+
+            await Wallet.ImportAsync(walletConfig, walletCredentails, importConfig);
+
+            // Open the wallet
+            using (var stewardWallet = await Wallet.OpenWalletAsync(walletConfig, walletCredentails))
+            {
+
+
+                // Retrieve stored key
+                var myKeys = await Did.ListMyDidsWithMetaAsync(stewardWallet);
+
+                // Compare the two keys
+                Console.WriteLine(Mykeys);
+
+                await stewardWallet.CloseAsync();
+            }
         }
     }
 }
