00d19ed746c31d355c26103a7a2bbbc1d503d53b
Sytseoegema <sy.oegema@oegematransport.nl>
2020-05-18 11:27:54 +0200

    fix

--- Initialize.cs
+++ Initialize.cs
@@ -3,7 +3,6 @@ using System.Threading.Tasks;
 
 using Newtonsoft.Json;
 
-using Hyperledger.Indy.PoolApi;
 
 namespace indyClient
 {
@@ -18,13 +17,33 @@ namespace indyClient
             try
             {
                 Console.WriteLine("reinitialize not implemented yet.");
-                await Pool.CreatePoolLedgerConfigAsync("reset", "{ \"genesis_txn\": \"/var/lib/indy/reset/\" }");
+                await Pool.CreatePoolLedgerConfigAsync("reset", "{}");
             }
             catch (Exception e)
             {
                 Console.WriteLine($"Error: {e.Message}");
             }
+        }
 
+        public asynce Task createGenesisWallets()
+        {
+            WalletController wallet = new WalletController();
+            DidController did = new DidController();
+
+            await wallet.create("Trustee1");
+            await wallet.open("Trustee1");
+            did.setOpenWallet(wallet.getOpenWallet());
+            await did.create("000000000000000000000000Trustee1");
+
+            await wallet.create("Steward1");
+            await wallet.open("Steward1");
+            did.setOpenWallet(wallet.getOpenWallet());
+            await did.create("000000000000000000000000Steward1");
+
+            await wallet.create("Steward2");
+            await wallet.open("Steward2");
+            did.setOpenWallet(wallet.getOpenWallet());
+            await did.create("000000000000000000000000Steward2");
         }
     }
 }
