67de4c8a835715bc563b3ee03db36a7886284af0
Sytseoegema <sy.oegema@oegematransport.nl>
2020-05-18 18:15:28 +0200

    test

--- Initialize.cs
+++ Initialize.cs
@@ -58,7 +58,7 @@ namespace indyClient
             did.setOpenWallet(wallet.getOpenWallet());
             var didJson = await did.create("");
 
-            Console.WriteLine(didJson);
+            Console.WriteLine(JObject.parse(didJson).did);
 
           // public async Task sendNymRequest(string trusteeName, string trusteeDid,
           // string did, string verkey ,string alias, string role)
