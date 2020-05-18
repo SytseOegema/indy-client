4bbe1910aea383451acc93af527d3086754c5086
Sytseoegema <sy.oegema@oegematransport.nl>
2020-05-18 10:05:20 +0200

    Wallet create, open, close

--- WalletController.cs
+++ WalletController.cs
@@ -11,60 +11,75 @@ namespace indyClient
 {
     class WalletController
     {
+        private string d_walletConfig;
+        private string d_walletCredentails;
+        private string d_identifier;
         private Wallet d_openWallet;
 
         public WalletController()
         {}
 
-        public async Task open()
+        public async Task create()
         {
-            Console.WriteLine("name of the wallet you would like to open:");
-            string identifier = Console.ReadLine();
-
-            string walletConfig = "{ \"id\": \"" + identifier + "\" }";
-            string walletCredentails = "{ \"key\": \"" + identifier + "\" }";
-
-            using (d_openWallet = await Wallet.OpenWalletAsync(walletConfig, walletCredentails))
-            {
-
-                Console.WriteLine("3");
-
-                // Retrieve stored key
-                var myKeys = await Did.ListMyDidsWithMetaAsync(d_openWallet);
-
-                Console.WriteLine("4");
-
-                // Compare the two keys
-                Console.WriteLine(myKeys);
-
-                await d_openWallet.CloseAsync();
-            }
-        }
-
-        public async Task import()
-        {
-            var walletConfig = "{\"id\":\"Steward1test\"}";
-            var walletCredentails = "{\"key\":\"Steward1test\"}";
-            var importConfig = JsonConvert.SerializeObject(new
-                {
-                    path = "/home/hyper/wallets/steward_wallet",
-                    key = "test"
-                });
+            Console.WriteLine("name of the wallet you would like to create:");
+            d_identifier = Console.ReadLine();
+            setWalletInfo();
 
             try
             {
-                Console.WriteLine("1");
-                await Wallet.ImportAsync(walletConfig, walletCredentails, importConfig);
-
-                Console.WriteLine("2");
-
-
+                await WalletUtils.CreateWalletAsync(firstWalletConfig, firstWalletCredentials);
             }
             catch (Exception e)
             {
                 Console.WriteLine($"Error: {e.Message}");
             }
+        }
 
+        public async Task open()
+        {
+            Console.WriteLine("name of the wallet you would like to open:");
+            d_identifier = Console.ReadLine();
+            setWalletInfo(d_identifier);
+
+            try
+            {
+              d_openWallet = await Wallet.OpenWalletAsync(d_walletConfig, d_walletCredentails);
+
+              // Retrieve stored key
+              var myKeys = await Did.ListMyDidsWithMetaAsync(d_openWallet);
+              Console.WriteLine("wallet " + d_identifier " opened");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+        }
+
+        public async Task close()
+        {
+            try
+            {
+                await d_openWallet.CloseAsync();
+                Console.WriteLine("wallet " + d_identifier " closed");
+                resetWalletInfo();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+        }
+
+        private setWalletInfo()
+        {
+            d_walletConfig = "{ \"id\": \"" + d_identifier + "\" }";
+            d_walletCredentails = "{ \"key\": \"" + d_identifier + "\" }";
+        }
+
+        privaet resetWalletInfo()
+        {
+          d_walletConfig = "";
+          d_walletCredentails = "";
+          d_identifier = "";
         }
     }
 }
