8eb4d39c1f598e35f93a90e17782ad8d28f0ac66
SytseOegema <sy.oegema@oegematransport.nl>
2020-05-22 19:11:52 +0200

    home path

--- Facilitators/IOFacilitator.cs
+++ Facilitators/IOFacilitator.cs
@@ -6,6 +6,9 @@ namespace indyClient
 {
     class IOFacilitator
     {
+var envHome = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "HOMEPATH" : "HOME";
+var home = Environment.GetEnvironmentVariable(envHome);`
+
         public void listDirectories(string path)
         {
             string [] files = Directory.GetDirectories(path);
