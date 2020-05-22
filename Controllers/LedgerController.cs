7ce821341923463164eb5b1ab4c8a155a944abb7
Sytseoegema <sy.oegema@oegematransport.nl>
2020-05-22 20:56:46 +0200

    schema list

--- Controllers/LedgerController.cs
+++ Controllers/LedgerController.cs
@@ -74,7 +74,7 @@ namespace indyClient
                 string recordJson = "{";
                 recordJson += "\"issuer_did\": \"" + issuerDid + "\",";
                 recordJson += "\"schema_id\": \"" + schema.SchemaId + "\",";
-                recordJson += "\"schema_name\": \"" + name + "\"";
+                recordJson += "\"schema_name\": \"" + name + "\",";
                 recordJson += "\"schema_version\": \"" + version + "\"";
                 recordJson += "}";
 
