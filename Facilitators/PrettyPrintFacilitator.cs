6e35b9312c4675659620335436e5e76a31bd2de3
Sytseoegema <sy.oegema@oegematransport.nl>
2020-05-25 15:42:33 +0200

    pretty print

--- Facilitators/PrettyPrintFacilitator.cs
+++ Facilitators/PrettyPrintFacilitator.cs
@@ -7,35 +7,38 @@ namespace indyClient
         public string dePrettyJsonMember(string json, string member)
         {
             int startIdx = json.IndexOf(member);
-            int openIdx = json.IndexOf('{', startIdx);
-            int openIdx2 = openIdx + 1;
-            int closeIdx = openIdx2;
-
-            while (true)
+            while(startIdx != -1)
             {
-                closeIdx = json.IndexOf('}', closeIdx);
-                // check if there is an object within the object
-                openIdx2 = json.IndexOf('{', openIdx2);
+                int openIdx = json.IndexOf('{', startIdx);
+                int openIdx2 = openIdx + 1;
+                int closeIdx = openIdx2;
 
-                Console.WriteLine("close: " + closeIdx);
-                Console.WriteLine("open: " + openIdx2);
-                if (openIdx2 == -1 ^ openIdx2 > closeIdx)
-                    break;
+                while (true)
+                {
+                  closeIdx = json.IndexOf('}', closeIdx);
+                  // check if there is an object within the object
+                  openIdx2 = json.IndexOf('{', openIdx2);
 
-                closeIdx++;
-                openIdx2++;
+                  Console.WriteLine("close: " + closeIdx);
+                  Console.WriteLine("open: " + openIdx2);
+                  if (openIdx2 == -1 ^ openIdx2 > closeIdx)
+                  break;
+
+                  closeIdx++;
+                  openIdx2++;
+                }
+
+                Console.WriteLine("begin:  \n" + begin);
+                Console.WriteLine("sub:  \n" + sub);
+                Console.WriteLine("end:  \n" + end);
+
+                sub = sub.Replace(" ", "");
+                sub = sub.Replace("\n", "");
+                json = begin + sub + end;
+                startIdx = json.IndexOf(member, closeIdx);
             }
 
-            string begin = json.Substring(0, openIdx);
-            Console.WriteLine("begin:  \n" + begin);
-            string sub = json.Substring(openIdx, closeIdx - openIdx + 1);
-            Console.WriteLine("sub:  \n" + sub);
-            string end = json.Substring(closeIdx + 1);
-            Console.WriteLine("end:  \n" + end);
-
-            sub = sub.Replace(" ", "");
-            sub = sub.Replace("\n", "");
-            return begin + sub + end;
+            return json;
         }
 
     }
