67d03cb78ec847aef5f1446645b7ed9318697659
Sytseoegema <sy.oegema@oegematransport.nl>
2020-06-02 12:15:04 +0200

    ipfs create file

--- Facilitators/IpfsFacilitator.cs
+++ Facilitators/IpfsFacilitator.cs
@@ -1,13 +1,16 @@
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using System.Net.Http;
 using Ipfs.Http;
 
 namespace indyClient
 {
     class IpfsFacilitator
     {
+        // private string d_baseUrl = "http://127.0.0.1:5001";
         static IpfsClient ipfs = new IpfsClient("http://127.0.0.1:5001");
+        private static readonly HttpClient client = new HttpClient();
         private IOFacilitator io = new IOFacilitator();
 
         public async Task test()
@@ -24,8 +27,11 @@ namespace indyClient
 
         public async Task getFile(string ipfsPath)
         {
-            Stream res = await ipfs.FileSystem.ReadFileAsync(ipfsPath);
-            io.createFile(res, "test");
+            var response = await client.PostAsync("https://postman-echo.com/post", null);
+            var responseString = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(responseString);
+            // Stream res = await ipfs.FileSystem.ReadFileAsync(ipfsPath);
+            // io.createFile(res, "test");
         }
     }
 }
