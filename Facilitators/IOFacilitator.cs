b08a0f3fc8d3424845d76a946697b651a92389a8
Sytseoegema <sy.oegema@oegematransport.nl>
2020-06-02 11:12:14 +0200

    ipfs create file

--- Facilitators/IOFacilitator.cs
+++ Facilitators/IOFacilitator.cs
@@ -17,6 +17,14 @@ namespace indyClient
             // /home/hyper/.indy_client
         }
 
+        public void createFile(stream data, string filename)
+        {
+            using (FileStream file = File.Create(d_homePath + "/" + filename))
+            {
+                data.CopyTo(file);
+            }
+        }
+
 
         public void listDirectories(string path)
         {
--- Facilitators/IpfsFacilitator.cs
+++ Facilitators/IpfsFacilitator.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Threading.Tasks;
 using Ipfs.Http;
 
@@ -7,6 +8,7 @@ namespace indyClient
     class IpfsFacilitator
     {
         static IpfsClient ipfs = new IpfsClient("http://127.0.0.1:5001");
+        private IOFacilitator io = new IOFacilitator();
 
         public async Task test()
         {
@@ -14,10 +16,16 @@ namespace indyClient
             Console.WriteLine(peer);
         }
 
-        public async Task addFile(string path)
+        public async Task<string> addFile(string localPath)
         {
-            var res = await ipfs.FileSystem.AddFileAsync(path);
-            Console.WriteLine(res.Id);
+            var res = await ipfs.FileSystem.AddFileAsync(localPath);
+            return res.Id;
+        }
+
+        public async Task getFile(string ipfsPath)
+        {
+            stream res = await ipfs.FileSystem.GetAsync(ipfsPath);
+            io.createFile(res, "test");
         }
     }
 }
