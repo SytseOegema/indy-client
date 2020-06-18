965d5b4129dd8f9f61036a79c309e9cb5838624e
Sytseoegema <sy.oegema@oegematransport.nl>
2020-06-18 15:02:54 +0200

    fix

--- Controllers/WalletController.cs
+++ Controllers/WalletController.cs
@@ -518,9 +518,14 @@ namespace indyClient
             string json = model.emergency_trusted_parties_schema;
             string schema_id = GovernmentSchemasModel.getSchemaId(json);
 
+            Console.WriteLine(json);
+            Console.WriteLine(schema_id);
+
             // return array with credentials json
             json = await getCredentials("{\"schema_id\": \"" + schema_id + "\"}");
+            Console.WriteLine(json);
             JArray a = JArray.Parse(json);
+            Console.WriteLine("json");
             for(int idx = 0; idx < a.Count; ++idx)
             {
                 JObject cred = (JObject) a[idx];
